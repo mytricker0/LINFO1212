4c2ddf6e92fc7fa9b635b7ecea2b716eae1f67f3
step <mytricker0@gmail.com>
2023-11-15 20:12:34 +0100

    changed locationn of js script

---
+++ scripts/verifypasswd.js
@@ -0,0 +1,20 @@
+function checkPassword(){
+    let password = document.getElementById
+    ("password").value;
+    let confirmPassword = document.getElementById
+    ("confirmPassword").value;
+    console.log(password,confirmPassword);
+    let message = document.getElementById
+    ("message");
+
+    if(password.length != 0){
+        if(password == confirmPassword){
+            message.textContent = "Passwords match";
+            message.style.backgroundColor = "#3ae374"
+        }
+        else{
+            message.textContent = "Passwords don't match";
+            message.style.backgroundColor = "#ff4d4d";
+        }
+    }
+}
